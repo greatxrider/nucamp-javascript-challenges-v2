61d20a17810a55ff1970f9c92b6ad7c1ab1f1a09
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-03-03 00:01:04 +0800

    update

---
+++ Number Guessing Game/number-guessing-game.js
@@ -0,0 +1,40 @@
+function runGame() {
+    const target = Math.floor(Math.random() * 100) + 1;
+    let guessString = '';
+    let guessNumber = 0;
+    let correct = false;
+    let numTries = 0;
+
+    do {
+
+    } while (!correct) {
+        guessString = prompt("Guess the number from 1-100.");
+        numTries++;
+        guessNumber = +guessString;
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
