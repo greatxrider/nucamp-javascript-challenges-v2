5ec8d1cc67be6577b7b30b04afd90e406ecf2647
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-03-09 13:16:31 +0800

    update new file

--- JSON Objects/json-objects-challenge.js
+++ JSON Objects/json-objects-challenge.js
@@ -1,29 +1,78 @@
 async function fetchHero() {
-    /* This line below is fetching data from a website API. You haven't learned how to do this just yet, so here's a short explanation of what it does:
-    - 'fetch' is the function that sends a query to the website url, in this case this website https://www.superheroapi.com.
-    - 'await' tells JavaScript that the answer won't come back immediately and it needs to wait for it.
-    - 'then' tells JavaScript what to do with the website answer when it comes back
-    - 'response' is what the website sends back. We then use the text() function to get the text result. Note that the json() function would return a well formed JSON JavaScript Object.
-    */
-  
-    const hero = await fetch("https://www.superheroapi.com/api.php/1079471338910705/731").then((response) => response.text());
-  
-    /**** Step 1: print in the console the value of 'hero'. Check that it is a JSON in a string format. ****/
-    // Add your code here
-    console.log(hero);
-    /**** Step 2: transform this string into a JavaScript Object and console.log it ****/
-    // Add your code here
-    hero = JSON.stringify(hero);
-    console.log(hero);
-    /**** Step 3: using the function alert, display the name and the eye color of the Hero ****/
-    // Add your code here
-    alert(`${hero["name"]}, ${hero["color"]}`)
-    /**** Step 4: create a new constant named 'myHero' following the same format. Same fields can be set as 'null' ****/
-    // Add your code here
-  
-    /**** Step 5: create a new 'heroes' array and add both 'hero' and 'myHero' to it. Then create a stringified version of the array it using the JSON.stringify function, and console.log() it. ****/
-    // Add your code here
-  }
-  
-  fetchHero();
-  
+  /* This line below is fetching data from a website API. You haven't learned how to do this just yet, so here's a short explanation of what it does:
+  - 'fetch' is the function that sends a query to the website url, in this case this website https://www.superheroapi.com.
+  - 'await' tells JavaScript that the answer won't come back immediately and it needs to wait for it.
+  - 'then' tells JavaScript what to do with the website answer when it comes back
+  - 'response' is what the website sends back. We then use the text() function to get the text result. Note that the json() function would return a well formed JSON JavaScript Object.
+  */
+
+  const hero = await fetch(
+    "https://www.superheroapi.com/api.php/1079471338910705/731"
+  ).then(
+    (response) => response.text() //arrow function - function
+  );
+
+  /**** Step 1: print in the console the value of 'hero'. Check that it is a JSON in a string format. ****/
+  // Add your code here
+  console.log(hero);
+  /**** Step 2: transform this string into a JavaScript Object and console.log it ****/
+  // Add your code here
+  let heroObject = JSON.parse(hero);
+  console.log(heroObject);
+  /**** Step 3: using the function alert, display the name and the eye color of the Hero ****/
+  // Add your code here
+  alert(
+    `Hero Name: ${heroObject["name"]}, Hero Eye Color: ${heroObject["appearance"]["eye-color"]}`
+  );
+  /**** Step 4: create a new constant named 'myHero' following the same format. Same fields can be set as 'null' ****/
+  // Add your code here
+  const myHero = {
+    response: "success",
+    id: "731",
+    name: "Zoom",
+    powerstats: {
+      intelligence: "50",
+      strength: "10",
+      speed: "100",
+      durability: "28",
+      power: "100",
+      combat: "28"
+    },
+    biography: {
+      "full-name": "Hunter Zolomon",
+      "alter-egos": "No alter egos found.",
+      aliases: ["-"],
+      "place-of-birth": "-",
+      "first-appearance": "Flash Secret Files #3",
+      publisher: "DC Comics",
+      alignment: "bad"
+    },
+    appearance: {
+      gender: "Male",
+      race: "null",
+      height: ["6'1", "185 cm"],
+      weight: ["181 lb", "81 kg"],
+      "eye-color": "Red",
+      "hair-color": "Brown"
+    },
+    work: {
+      occupation: "-",
+      base: "Keystone City, Kansas"
+    },
+    connections: {
+      "group-affiliation":
+        "Secret Society of Super Villains, formerly Keystone Police Department, F.B.I.",
+      relatives: "Ashley Zolomon (ex-wife)"
+    },
+    image: {
+      url: "https://www.superherodb.com/pictures2/portraits/10/100/1384.jpg"
+    }
+  };
+  /**** Step 5: create a new 'heroes' array and add both 'hero' and 'myHero' to it. Then create a stringified version of the array it using the JSON.stringify function, and console.log() it. ****/
+  // Add your code here
+  const heroes = [hero, myHero];
+  const heroesJson = JSON.stringify(heroes);
+  console.log(heroesJson);
+}
+
+fetchHero();
